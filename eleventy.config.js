bf4aac2a01d38d1b7452284781b6df41166b7428
Leonardo Silveira <sombriks@gmail.com>
2025-01-15 10:22:00 -0300

    pug is funny

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,10 @@
+import pugPlugin from "@11ty/eleventy-plugin-pug"
+
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default async function (eleventyConfig) {
   // Configure Eleventy
+  eleventyConfig.addPlugin(pugPlugin)
   eleventyConfig.setInputDirectory("src/pages")
+  eleventyConfig.setIncludesDirectory("../includes")
   eleventyConfig.setOutputDirectory("dist")
 }
